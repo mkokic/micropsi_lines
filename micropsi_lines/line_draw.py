6913578f8f050b37686bb6f0040f8043faecca0b
Mia Kokic <mkokic65@gmail.com>
2020-09-29 17:41:16 +0200

    final

--- micropsi_lines/line_draw.py
+++ micropsi_lines/line_draw.py
@@ -2,55 +2,74 @@
 import numpy as np
 from PIL import Image
 import os
+from IPython import embed
 
 
 def line_params(x1, y1, x2, y2):
+    # For vertical line compute only points on y-axis
     if x1 == x2:
         points = np.array([np.ones(abs(y2 - y1) + 1, ) * x1,
                            np.linspace(y1, y2, abs(y2 - y1) + 1)]).astype(np.int64)
+
+    # For all the other cases compute slope and intercept
     else:
+        # Slope
         a = (y2 - y1) / (x2 - x1)
+        # Inctercept
         b = y1 - a * x1
-        x_r2g = np.linspace(x1, x2, abs(x2 - x1) + 1)
-        points = np.array([x_r2g,
-                           a * x_r2g + b]).astype(np.int64)
+
+        # Generate points on a line
+        xs = np.linspace(x1, x2, abs(x2 - x1) + 1)
+        points = np.array([xs, a * xs + b]).astype(np.int64)
     return points
 
 
 class LineDraw(object):
     def __init__(self):
+        # Load an image from the data folder
         self.path = os.path.dirname(__file__) + '/data/'
         self.img = Image.open(self.path + 'mpsi_task.png').convert("RGB")
+
+        # Extract pixels coordinates
         self.pix_coords = self.get_pix(self.img)
+        # Compute lines connecting the pixels
         self.lines = self.compute_lines(self.pix_coords)
+        # Draw the lines on the input image and save
         self.img_out = self.draw_lines(self.img, self.lines)
 
     @staticmethod
     def get_pix(img):
+        # Get coordinates of the red, green and blue pixel
         rgb_vals = (np.eye(3) * 255).astype(np.uint8)
         pix_coords = np.array(
             [list(divmod([i for i, pixel in enumerate(img.getdata()) if pixel == tuple(col)][0], img.size[0])) for col
-             in rgb_vals]).reshape(-1, 2)
+             in rgb_vals]).reshape(3, 2)
+
         return pix_coords
 
     @staticmethod
     def compute_lines(pix_coords):
+        # Get coordinates of the three pixels
         x1, y1 = pix_coords[0, 0], pix_coords[0, 1]
         x2, y2 = pix_coords[1, 0], pix_coords[1, 1]
         x3, y3 = pix_coords[2, 0], pix_coords[2, 1]
 
+        # Generate points on the lines connecting three pixels
         points_r2g = line_params(x1, y1, x2, y2)
         points_g2b = line_params(x2, y2, x3, y3)
         points_r2b = line_params(x1, y1, x3, y3)
-
         lines = np.concatenate((points_r2g, points_g2b, points_r2b), 1)
+
         return lines
 
+    # Draw the lines on the input image
     def draw_lines(self, img, lines):
         img_arr = np.array(img)
         img_arr[lines[0], lines[1], :] = [255, 255, 255]
         img_out = Image.fromarray(img_arr, 'RGB')
+        # Save the new image
         img_out.save(self.path + 'mpsi_task_out.png')
+
         return img_out
 
 
--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import unittest
 import numpy as np
-from micropsi_lines.line_draw import LineDraw
 from PIL import Image
-from IPython import embed
+from micropsi_lines.line_draw import LineDraw
 
 
 class MyTestCase(unittest.TestCase):
@@ -14,70 +13,84 @@ class MyTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
+    # Test the mode and the size of the input image
     def test_img_in(self):
         self.assertEqual(self.ld.img.mode, "RGB")
         self.assertEqual(self.ld.img.size, (64, 64))
 
+    # Test that extracted pixels are an int array of 3x2 points between 0 and 64
     def test_pix(self):
         self.assertIs(self.ld.pix_coords.dtype, np.dtype('int64'))
         self.assertIsInstance(self.ld.pix_coords, np.ndarray)
         self.assertEqual(np.unique(self.ld.pix_coords, axis=0).shape, (3, 2))
         self.assertTrue(0 <= self.ld.pix_coords.all() < 64)
 
+    # Test that the coordinates and colors of extracted pixels values are correct
     def test_get_pix(self):
+        # Expected image with pixels in specified locations
         img_exp = Image.new("RGB", (64, 64))
-        img_exp.putpixel((10, 10), (255, 0, 0))
-        img_exp.putpixel((30, 30), (0, 255, 0))
-        img_exp.putpixel((50, 50), (0, 0, 255))
+        img_exp.putpixel((10, 10), (255, 0, 0))  # Red
+        img_exp.putpixel((30, 30), (0, 255, 0))  # Green
+        img_exp.putpixel((50, 50), (0, 0, 255))  # Blue
 
+        # Extract pixels coordinates from the expected image
         pix_coords_actual = self.ld.get_pix(img_exp)
+
+        # Generate an actual image with computed pixel coordinates
         img_actual = Image.new("RGB", (64, 64))
         rgb_vals = (np.eye(3) * 255).astype(np.int64)
         for p, c in zip(pix_coords_actual, rgb_vals):
             img_actual.putpixel(tuple(p), tuple(c))
+
         self.assertEqual(img_actual, img_exp)
 
+    # Test that lines are an array of integers
     def test_lines(self):
         self.assertIs(self.ld.lines.dtype, np.dtype('int64'))
         self.assertIsInstance(self.ld.lines, np.ndarray)
 
+    # Test that the lines computed from pixels are correct
     def test_compute_lines(self):
         # Test vertical case
+        # x1 = x2 = x3; y1 = 10, y2 = 30, y3 = 50
         pix_coords_exp_vert = np.array([[10, 10],
                                         [10, 30],
                                         [10, 50]])
-        lines_exp_vert = np.concatenate([np.array([10 * np.ones(21, ), np.linspace(10, 30, 21)]).astype(np.int64),
-                                         np.array([10 * np.ones(21, ), np.linspace(30, 50, 21)]).astype(np.int64),
-                                         np.array([10 * np.ones(41, ), np.linspace(10, 50, 41)]).astype(np.int64)], 1)
+        lines_exp_vert = np.concatenate([np.array([[10] * 21, np.linspace(10, 30, 21)]),
+                                         np.array([[10] * 21, np.linspace(30, 50, 21)]),
+                                         np.array([[10] * 41, np.linspace(10, 50, 41)])], 1).astype(np.int64)
 
         lines_actual_vert = self.ld.compute_lines(pix_coords_exp_vert)
         self.assertEqual(lines_actual_vert.tolist(), lines_exp_vert.tolist())
 
         # Test all the other cases
+        # x1 = 10, x2 = 30, x3 = 50; y1 = 10, y2 = 10, y3 = 10
         pix_coords_exp = np.array([[10, 10],
                                    [30, 10],
                                    [50, 10]])
-        lines_exp = np.concatenate([np.array([np.linspace(10, 30, 21), 10 * np.ones(21, )]).astype(np.int64),
-                                    np.array([np.linspace(30, 50, 21), 10 * np.ones(21, )]).astype(np.int64),
-                                    np.array([np.linspace(10, 50, 41), 10 * np.ones(41, )]).astype(np.int64)], 1)
+        lines_exp = np.concatenate([np.array([np.linspace(10, 30, 21), [10] * 21]),
+                                    np.array([np.linspace(30, 50, 21), [10] * 21]),
+                                    np.array([np.linspace(10, 50, 41), [10] * 41])], 1).astype(np.int64)
 
         lines_actual = self.ld.compute_lines(pix_coords_exp)
         self.assertEqual(lines_actual.tolist(), lines_exp.tolist())
 
-    def test_img_out_lines(self):
-        img_out = self.ld.draw_lines(self.ld.img, self.ld.lines)
-        self.assertEqual(img_out.mode, "RGB")
-        self.assertEqual(img_out.size, (64, 64))
-
+    # Test the mode and the size of the output image
     def test_img_out(self):
-        img_expected = Image.new("RGB", (64, 64))
-        lines_expected = np.hstack([np.array([np.linspace(10, 20, 11), 10 * np.ones(11, )]).astype(np.int64),
-                                    np.array([np.linspace(20, 30, 11), 10 * np.ones(11, )]).astype(np.int64)])
+        self.assertEqual(self.ld.img_out.mode, "RGB")
+        self.assertEqual(self.ld.img_out.size, (64, 64))
 
-        img_actual = self.ld.draw_lines(img_expected, lines_expected)
+    # Test that the output image contains white lines connecting the pixels
+    def test_draw_lines(self):
+        img_exp = Image.new("RGB", (64, 64))
+        lines_exp = np.concatenate([np.array([np.linspace(10, 30, 21), [10] * 21]),
+                                    np.array([np.linspace(30, 50, 21), [10] * 21]),
+                                    np.array([np.linspace(10, 50, 41), [10] * 41])], 1).astype(np.int64)
 
-        img_expected_arr = np.array(img_expected)
-        img_expected_arr[lines_expected[0], lines_expected[1], :] = [255, 255, 255]
+        img_actual = self.ld.draw_lines(img_exp, lines_exp)
+
+        img_expected_arr = np.array(img_exp)
+        img_expected_arr[lines_exp[0], lines_exp[1], :] = [255, 255, 255]
         img_expected = Image.fromarray(img_expected_arr, 'RGB')
         self.assertEqual(img_actual, img_expected)
 
